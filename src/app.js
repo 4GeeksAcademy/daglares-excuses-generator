6e7726ad405d3bb5e04b2732cd7efe767574d7c5
DAGarciaLopez <dagarcialopez5@gmail.com>
2024-12-10 21:31:51 +0000

    Generador de excusas 2.0

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ window.onload = function() {
     "en la noche",
     "mientras hacía ejercicio",
     "mientras comía",
-    "mientras rezaba",
+    "mientras rezaba"
   ];
 
   function random(list) {
@@ -33,3 +33,49 @@ window.onload = function() {
   excuseGenerator.textContent = excuse;
   console.log(excuseGenerator);
 };
+
+function pickRandom(lista) {
+  let randomNumber = Math.random() * lista.length;
+  let roundedNumber = Math.floor(randomNumber);
+  return lista[roundedNumber];
+}
+
+function generateRandomColor() {
+  let color = "#";
+
+  const valoresHexadecimales = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  ];
+
+  for (let index = 0; index < 6; index++) {
+    color += pickRandom(valoresHexadecimales); // concatenar
+  }
+  return color;
+}
+
+let newColor = generateRandomColor();
+let bodyElement = document.querySelector("body");
+bodyElement.style.backgroundColor = newColor;
+
+const refreshButton = document.querySelector(".refresh-button");
+
+const refreshPage = () => {
+  location.reload();
+};
+
+refreshButton.addEventListener("click", refreshPage);

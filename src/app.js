62823de13f09d0159434dbcc07bb17d874864aee
DAGarciaLopez <dagarcialopez5@gmail.com>
2024-12-10 02:35:21 +0000

    gereador de excusas 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["Mi perro ", "Luis Miguel ", "Mi abuela ", "Mi canario "];
+
+  let action = ["se comió ", "orinó ", "robó ", "rompió "];
+
+  let what = ["mi tarea ", "mi teléfono ", "el coche "];
+
+  let when = [
+    "antes de clase",
+    "en la noche",
+    "mientras hacía ejercicio",
+    "mientras comía",
+    "mientras rezaba",
+  ];
+
+  function random(list) {
+    let randomNumber = Math.floor(Math.random() * list.length);
+    return list[randomNumber];
+  }
+
+  let excuse = random(who) + random(action) + random(what) + random(when);
+
+  let excuseGenerator = document.querySelector("#excuseGenerator");
+  excuseGenerator.textContent = excuse;
+  console.log(excuseGenerator);
 };
